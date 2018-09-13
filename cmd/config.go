75f2ac29011773d51aa7a15759e2e664ea896fd4
Kyle  McEwen <kylemcewen@Kyles-MacBook-Pro.local>
2018-09-12 21:55:57 -0400

    Commands and Flags

--- cmd/config.go
+++ cmd/config.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var configCmd = &cobra.Command{
-	Use: "config",
+	Use:   "config",
+	Short: "Configure docapp",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hello from config")
 	},
 }
 
 func init() {
+	//var Config string
 	rootCmd.AddCommand(configCmd)
+	rootCmd.PersistentFlags().StringP("config", "c", "", "config of docapp")
+	viper.BindPFlag("config", rootCmd.PersistentFlags().Lookup("config"))
 }
--- go.mod
+++ go.mod
@@ -2,5 +2,5 @@ module github.com/kwmcewen/docapp
 
 require (
 	github.com/spf13/cobra v0.0.3
-	github.com/spf13/pflag v1.0.2 // indirect
+	github.com/spf13/viper v1.2.0
 )
